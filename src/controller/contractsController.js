05562a74e279402679fefe3d7344325070c02d72
Adyson Lucas Bernardo Queiroga <adyson.lucas@DCX.UFPB.BR>
2019-03-18 11:06:30 -0300

    get one on contract controller

--- src/controller/contractsController.js
+++ src/controller/contractsController.js
@@ -1,48 +1,59 @@
 const express = require('express');
 const router = express.Router();
-const sequelize = require('sequelize');
 const Contract = require('../models/contract');
 
 
-router.get('/listContracts', function(req, res) {
+const url = '/contract';
+
+router.get(url, function(req, res) {
     Contract.findAll().then(contracts => {
         res.send(contracts);
     })
   });
 
-router.post('/createContract', function(req, res){
-    try{
+router.get(url + '/:contract_id', function(req, res) {
+    Contract.findById(req.params.contract_id).then(contract => {
+        if (contract) {
+            res.send(contract);
+        } else {
+            res.json({error: 'contract not found'});
+        }
+    });
+});
+
+router.post(url, function(req, res) {
+    try {
         console.log(req.body);
         const contract = Contract.create(req.body);
         return res.send(req.body);
-    }catch (err){
+    } catch (err) {
         return res.status(400).send({error: 'falha no registro'});
     }
     
 });
 
-router.put('/updateContract', function(req,res){
-    Contract.findById(req.body.id).then(contract => {
-        if(contract){
+router.put(url + '/:contract_id', function(req,res) {
+    Contract.findById(req.params.contract_id).then(contract => {
+        if (contract) {
             contract.update(req.body).then(() => {
                 res.send(contract);
-            })
-        }else{
-            res.json({error: "user not found"});
+            });
+        } else {
+            res.json({error: 'user not found'});
         }
     });
 });
 
-router.delete('/deleteContract', function(req,res){
-    Contract.findById(req.body.id).then(contract => {
-        if(contract){
+router.delete(url + '/contract/:contract_id', function(req,res) {
+    Contract.findById(req.params.contract_id).then(contract => {
+        if (contract) {
             contract.destroy().then(() => {
                 res.send(contract);
-            })
-        }else{
-            res.json({error: "user not found"});
+            });
+        } else {
+            res.json({error: 'contract not found'});
         }
     });
 });
 
-module.exports = app => app.use('/contract', router);
+module.exports = app => app.use(url, router);
