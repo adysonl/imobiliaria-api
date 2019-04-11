507f565ccdae182d95df4fe16d72543e0eaaccb3
gabiandrade <gabriela.vitoria@dce.ufpb.br>
2019-04-11 02:16:35 -0300

    models de endereço

---
+++ src/models/address.js
@@ -0,0 +1,23 @@
+const Address = sequelize.define('addresses', {
+    street: {
+        type: Sequelize.STRING
+    },
+    streetNumber: {
+        type: Sequelize.STRING
+    },
+    neighbour: {
+        type: Sequelize.STRING
+    },
+    city: {
+        type: Sequelize.STRING
+    },
+    state: {
+        type: Sequelize.STRING
+    },
+    country: {
+        type: Sequelize.STRING
+    }
+});
+
+Address.sync();
+module.exports = Address;
